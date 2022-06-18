6a4d9d2bb8d792574b276a961a849b008ec3c115
Michael F. Schönitzer <michael@schoenitzer.de>
2022-06-18 15:23:05 +0200

    Allow to configure how entires should be merged

--- __init__.py
+++ __init__.py
@@ -1,15 +1,23 @@
 from anki.notes import Note
-from aqt import mw
-from aqt.utils import tooltip, qconnect
-from aqt.qt import *
+from aqt import gui_hooks, mw
 from aqt.browser import Browser
-from aqt import gui_hooks
+from aqt.qt import *
+from aqt.utils import qconnect, showCritical, tooltip
+
+config = mw.addonManager.getConfig(__name__)
 
 
-def merge_notes(note0: Note, note: Note) -> None:
+def merge_notes(note0: Note, note: Note, mode: str) -> None:
     for f in note.keys():
-        if f in note0 and len(note[f]) > len(note0[f]):
-            note0[f] = note[f]
+        if f in note0 and note0[f] != note[f]:
+            if mode == "concat":
+                note0[f] += note[f]
+            elif mode == "longer" and len(note[f]) > len(note0[f]):
+                note0[f] = note[f]
+            elif mode == "skip":
+                raise ValueError("Conflicting entries")
+            else:
+                raise NotImplementedError("Unknown mode %s" % mode)
 
     for t in note.tags:
         if not note0.hasTag(t):
@@ -19,13 +27,14 @@ def merge_notes(note0: Note, note: Note) -> None:
 
 
 def merge_duplicates(browser: Browser) -> None:
+    mode = config.get("merge_mode", "longer")
     cardids = browser.selectedCards()
     cards = [mw.col.getCard(id) for id in cardids]
     notes = []
 
     for card in cards:
         note = card.note()
-        if note.id not in [n.id for n,_ in notes]:
+        if note.id not in [n.id for n, _ in notes]:
             notes.append((note, card.reps))
 
     dups: dict[list[Note]] = {}
@@ -38,10 +47,16 @@ def merge_duplicates(browser: Browser) -> None:
 
     del_note_ids = []
     for dupl in dups.values():
-        dupl = [note for note, reps in sorted(dupl, key=lambda x: -x[1])] 
+        dupl = [note for note, reps in sorted(dupl, key=lambda x: -x[1])]
         for i in range(1, len(dupl)):
-            merge_notes(dupl[0], dupl[i])
-            del_note_ids.append(dupl[i].id)
+            try:
+                merge_notes(dupl[0], dupl[i], mode)
+                del_note_ids.append(dupl[i].id)
+            except ValueError:
+                pass
+            except NotImplementedError:
+                showCritical("Invalid configuration", parent=browser)
+                return
 
     browser.mw.col.remNotes(del_note_ids)
 
